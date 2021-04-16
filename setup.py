45ec7821f71c3f1abdfb9978ab6eb34b92b5bce9
M Hadi Sasmito <keselyoleren@gmail.com>
2021-04-16 15:22:52 +0700

    edit setup file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
  
 setup(
     name = 'django dummy user',
-    version ='0.1',
+    version ='0.1.0',
     packages=find_packages(),
     include_package_data = True,
     license = 'BSD License',
@@ -27,9 +27,7 @@ setup(
         'License :: OSI Approved :: BSD License', # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
     ]
 )
